2f02e4f8068d2d498f76e8b7797f608bf4ba4008
hykw <hitoshi-hayakawa@mail.bm-sms.co.jp>
2014-11-28 15:42:22 +0900

    backlog url間違い

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (tab.url.indexOf('sms-jinzai.backlog.jp') != -1) {
+    if (tab.url.indexOf('backlog.jp') != -1) {
         chrome.pageAction.show(tabId);
     }
 });
