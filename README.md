bbb527258d982c9ab2eb7f4f28d392dbd776632d
Hitoshi Hayakawa <hykw1234@gmail.com>
2014-11-29 11:26:39 +0900

    チケット追加時の期限日も対象にするようにした

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ https://chrome.google.com/webstore/detail/hykw-backlog-quick-button/oomhdjimigih
 1.1
     *.backlog.jp/(add|view) でのみアイコンを表示
     期限日に「次の月曜日」を追加、-1/-7 を削除
+    チケット追加時の期限日も対象にするようにした
 
 1.0.1
     特定のbacklogでしか動かないようになっていたのを修正
--- src/inject.js
+++ src/inject.js
@@ -6,11 +6,22 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     var changeEvent = document.createEvent('HTMLEvents');
     changeEvent.initEvent('change', true, true);
 
-    document.getElementsByName('switchStatusIssue.limitDate')[0].value = strdate;
-    document.getElementsByName('switchStatusIssue.limitDate')[1].value = strdate;
+    var obj_limitdate = document.getElementsByName('switchStatusIssue.limitDate');
 
-    document.getElementsByName('switchStatusIssue.limitDate')[0].dispatchEvent(changeEvent);
-    document.getElementsByName('switchStatusIssue.limitDate')[1].dispatchEvent(changeEvent);
+    // チケット追加
+    if (obj_limitdate.length == 0) {
+      var obj_limitdate = document.getElementById('limitDate');
+
+      obj_limitdate.value = strdate;
+      obj_limitdate.dispatchEvent(changeEvent);
+    } else {
+    // チケット変更
+      obj_limitdate[0].value = strdate;
+      obj_limitdate[1].value = strdate;
+
+      obj_limitdate[0].dispatchEvent(changeEvent);
+      obj_limitdate[1].dispatchEvent(changeEvent);
+    }
   }
 });
 
