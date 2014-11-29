0da9555fdc9291afe412d4111b921efeade553d5
Hitoshi Hayakawa <hykw1234@gmail.com>
2014-11-29 10:33:05 +0900

    *.backlog.jp/(add|view) でのみアイコンを表示

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 chrome_hykwBacklogQuickButton
 =============================
 
-Extension name: HYKW Backlog Quick button
+# HYKW Backlog Quick button
+## URL
+https://chrome.google.com/webstore/detail/hykw-backlog-quick-button/oomhdjimigihpepifblefcclaglilabf
 
-URL: https://chrome.google.com/webstore/detail/hykw-backlog-quick-button/oomhdjimigihpepifblefcclaglilabf
+## History
 
+1.1
+    *.backlog.jp/(add|view) でのみアイコンを表示
+
+1.0.1
+    特定のbacklogでしか動かないようになっていたのを修正
+
+1.0.0
+    初期バージョン
--- src/background.js
+++ src/background.js
@@ -1,7 +1,19 @@
-
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (tab.url.indexOf('backlog.jp') != -1) {
-        chrome.pageAction.show(tabId);
+  var showFlag = false;
+
+  var allowDomains = Array(
+    'backlog.jp/view',
+    'backlog.jp/add'
+  );
+
+  for (var i = 0; i < allowDomains.length; i++) {
+    if (tab.url.indexOf(allowDomains[i]) > -1) {
+      showFlag = true;
+      break;
     }
+  }
+
+  if (showFlag == true)
+    chrome.pageAction.show(tabId);
 });
 
